c6fa4db48646b02c085fa2c32153ef7579819dfb
fjfdepedro <fjfdepedro@gmail.com>
2015-02-21 00:34:52 +0100

    models new fields

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,36 @@
 class Author
-  include Mongoid::Document
-  field :name, type: String
-  field :date, type: String  
+	include Mongoid::Document
+	field :name, type: String
+	field :alternate_names: Array
+	field :birth_date, type: String  
+	field :death_date, type: String  
+	field :fuller_name, type: String  
+	field :links, type: Array
+	field :location, type: String  
+	field :publishers, type: Array
+	field :website, type: String  
+	field :wikipedia, type: String  
+bio
+
+
+	borrar:
+	"date",
+	"comment",
+	"dewey_decimal_class",
+	"entity_type",
+	"lc_classifications",
+	"lccn",
+	"languages",
+	"last_modified",
+	"latest_revision",
+	marc
+	personal_name
+	"publish_country",
+	"publish_date",
+	"publish_places",
+	subtitle
+	tags
+	title_prefix
+
+
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,5 +2,10 @@ class Book
   include Mongoid::Document
   has_and_belongs_to_many :authors
   field :title, type: String
-  field :description, type: String
+  field :subtitle, type: String
+  field :descriptiontext, type: String
+  field :subjects: Array
+  field :links, type: Array
+  field :subject_places: Array
+  field :subject_times: Array  
 end
